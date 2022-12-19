21ebeed3f1f7e694abccc51ee1f3e2a6177f01e5
ElenaMLopez <elena@holafly.com>
2022-12-19 14:00:00 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Elena Mateos López on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
