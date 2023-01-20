796df5aa0ce6ea331daa34ad122a32e8da0505ae
ElenaMLopez <elena@holafly.com>
2023-01-20 12:28:00 +0100

    Add a custo Search Bar

--- Weather/Views/Detail/WeatherView.swift
+++ Weather/Views/Detail/WeatherView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct WeatherView: View {
+    @State private var searchText = ""
+    
+    var searchResults: [Forecast] {
+        if searchText.isEmpty {
+            return Forecast.cities
+        } else {
+            return Forecast.cities.filter { $0.location.contains(searchText)}
+        }
+    }
     var body: some View {
         ZStack {
             Color.background
@@ -16,9 +25,8 @@ struct WeatherView: View {
             // MARK: Weather Widgets
             ScrollView(showsIndicators: false) {
                 VStack(spacing: 20) {
-                    ForEach(Forecast.cities) { forecast in
+                    ForEach(searchResults) { forecast in
                         WeatherWidget(forecast: forecast)
-                        
                     }
                 }
             }
@@ -29,16 +37,20 @@ struct WeatherView: View {
             }
         }
         .overlay {
-            NavigationBar()
+            // MARK: Navigation Bar
+            NavigationBar(searchText: $searchText)
         }
         .navigationBarHidden(true)
-        
+
+//        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a city or airport")
     }
 }
 
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
-        WeatherView()
-            .preferredColorScheme(.dark)
+        NavigationView {
+            WeatherView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
--- Weather/Views/Navigation/NavigationBar.swift
+++ Weather/Views/Navigation/NavigationBar.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct NavigationBar: View {
     @Environment(\.dismiss) var dismiss
-    
+    @Binding var searchText: String
     var body: some View {
         VStack(spacing: 8) {
             HStack {
@@ -34,6 +34,17 @@ struct NavigationBar: View {
                 .frame(width: 44, height: 44, alignment: .trailing)
             }
             .frame(height: 52)
+            // MARK: Search Bar
+            HStack(spacing: 2) {
+                Image(systemName: "magnifyingglass")
+                TextField("Search for a city or airport", text: $searchText)
+            }
+            .foregroundColor(.secondary)
+            .padding(.horizontal, 6)
+            .padding(.vertical, 7)
+            .frame(height: 36, alignment: .leading)
+            .background(Color.bottomSheetBackground, in: RoundedRectangle(cornerRadius: 10))
+            .innerShadow(shape: RoundedRectangle(cornerRadius: 10), color: .black.opacity(0.25), lineWidth: 2, offsetX: 0, offsetY: 2, blur: 2)
         }
         .frame(height: 106, alignment: .top)
         .padding(.horizontal, 16)
@@ -47,6 +58,6 @@ struct NavigationBar: View {
 
 struct NavigationBar_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationBar()
+        NavigationBar(searchText: .constant("Search for a city or airport"))
     }
 }
